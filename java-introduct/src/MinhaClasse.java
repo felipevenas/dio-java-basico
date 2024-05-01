93c1ba6f4194bd7fc8445499d45230c4a6ebf4fb
felipevenas <dev.felipevenas@gmail.com>
2024-04-30 22:42:11 -0300

    QuartoCommit

--- java-introduct/src/MinhaClasse.java
+++ java-introduct/src/MinhaClasse.java
@@ -1,11 +1,14 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        String nomeAluno;
-        nomeAluno = "Felipe";
+
+        String meuNome;
+        meuNome = "Felipe";
+        String meuEstado;
         final String BR = "Brasil"; // Regrinha básica: Quando uma variável for nomeada toda em maiúsculo, podemos supor que ela é do tipo 'final', ou seja, não poderá sofrer alterações durante o código.
         final int ESTADOS_BR = 27; // Mais um exemplo de uma variável imutável e sua sintaxe padrão recomendada.
 
-        System.out.println("O " + BR + " possui " + ESTADOS_BR + " estados.");
-        System.out.println("Nome do aluno é " + nomeAluno + " e ele reside no " + BR + ".");
+            System.out.println("Nome do aluno é " + meuNome + " e ele reside no " + BR + ".");
+            System.out.println("O " + BR + " possui " + ESTADOS_BR + " estados."); 
+
     }
 }
