f2adc222758afa85aa94de0aed7430a8176f9c69
felipevenas <dev.felipevenas@gmail.com>
2024-04-30 22:51:26 -0300

    Concat - nomeCompleto

--- dio-concat/src/MinhaClasse.java
+++ dio-concat/src/MinhaClasse.java
@@ -2,13 +2,14 @@ public class MinhaClasse {
     public static void main(String[] args) throws Exception {
         String primeiroNome = "Felipe";
         String segundoNome = "Venas"; 
+        String terceiroNome = "Souza";
 
-        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome, terceiroNome);
 
         System.out.println(nomeCompleto);
     }
 
-    public static String nomeCompleto (String primeiroNome, String segundoNome) {
-        return primeiroNome.concat(" ").concat(segundoNome);
+    public static String nomeCompleto (String primeiroNome, String segundoNome, String terceiroNome) {
+        return primeiroNome.concat(" " + segundoNome).concat(" " + terceiroNome);
     }
 }
