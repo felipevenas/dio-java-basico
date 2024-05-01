7689c6adcdc372bf7ec14c8d06c73229e9587563
felipevenas <dev.felipevenas@gmail.com>
2024-04-30 22:23:21 -0300

    SegundoCommit

---
+++ java-introduct/src/MinhaClasse.java
@@ -0,0 +1,8 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String nome;
+        nome = "Felipe";
+
+        System.out.println("Meu nome é: " + nome);
+    }
+}
